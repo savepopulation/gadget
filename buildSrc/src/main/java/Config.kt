a080e92fb1ce4bcf591ded96e165bb43fcb05689
tyln <taylan.sbrcn@gmail.com>
2021-04-28 23:12:06 +0300

    config updated

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,10 +4,10 @@
  */
 object Config {
     const val applicationId = "com.raqun.gadget"
-    //const val minSdkVersion = Versions.minSdkVersion
-    //const val targetSdkVersion = Versions.targetSdkVersion
-    //const val compileSdkVersion = Versions.compileSdkVersion
-    //const val buildToolsVersion = Versions.buildToolsVersion
+    const val minSdkVersion = Versions.minSdkVersion
+    const val targetSdkVersion = Versions.targetSdkVersion
+    const val compileSdkVersion = Versions.compileSdkVersion
+    const val buildToolsVersion = Versions.buildToolsVersion
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val versionCode = 1
     const val versionName = "1.0.0"
