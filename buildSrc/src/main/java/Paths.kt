1f3c1eb2f39f92268a7b19316db8298d80d195d5
tyln <taylan.sbrcn@gmail.com>
2021-04-30 15:21:16 +0300

    paths added

---
+++ buildSrc/src/main/java/Paths.kt
@@ -0,0 +1,11 @@
+/*
+ * Contains the path variables that used in project
+ */
+
+object Paths {
+    const val gradleClasspath = "com.android.tools.build:gradle:${Versions.gradleVersion}"
+    const val kotlinGradlePluginClasspath =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val googleServicesClasspath =
+        "com.google.gms:google-services:${Versions.googleServicesPluginVersion}"
+}
