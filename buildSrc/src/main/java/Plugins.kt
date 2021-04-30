43b607301bd356b336fdf247201c313525166efa
tyln <taylan.sbrcn@gmail.com>
2021-04-30 15:22:15 +0300

    plugins added

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,14 @@
+/*
+ * Contains the plugins that used in the project
+ */
+
+object Plugins {
+    const val androidApplication = "com.android.application"
+    const val androidDynamicFeature = "com.android.dynamic-feature"
+    const val androidLibrary = "com.android.library"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+    const val kotlinKapt = "kotlin-kapt"
+    const val detekt = "io.gitlab.arturbosch.detekt"
+    const val googleServices = "com.google.gms.google-services"
+}
