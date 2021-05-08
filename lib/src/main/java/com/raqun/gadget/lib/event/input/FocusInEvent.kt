ff4bf94fb927bec45f9378dd084cdc1f14517b8f
tyln <taylan.sbrcn@gmail.com>
2021-05-08 10:32:21 +0300

    focus in event refactored

--- lib/src/main/java/com/raqun/gadget/lib/event/input/FocusInEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/input/FocusInEvent.kt
@@ -2,5 +2,11 @@ package com.raqun.gadget.lib.event.input
 
 import com.raqun.gadget.lib.Constants
 
-data class FocusInEvent(private val eventName: String = Constants.Event.FOCUS_IN) :
-    InputEvent(eventName)
+/*
+ * Generic input area focus in event
+ */
+data class FocusInEvent(
+    private val eventName: String = Constants.Event.FOCUS_IN,
+    private val isValid: Boolean? = null
+) : InputEvent(eventName, isValid)
+
