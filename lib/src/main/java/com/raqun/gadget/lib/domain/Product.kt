41b46713930c72b03a0b1a640ef6ead30c4b7008
tyln <taylan.sbrcn@gmail.com>
2021-05-11 19:23:22 +0300

    product class added

---
+++ lib/src/main/java/com/raqun/gadget/lib/domain/Product.kt
@@ -0,0 +1,15 @@
+package com.raqun.gadget.lib.domain
+
+/*
+ * Product data class that will be tracked wit EEC Events
+ */
+data class Product(
+    val id: String?,
+    val name: String?,
+    val category: String?,
+    val variant: String?,
+    val brand: String?,
+    val price: Double?,
+    val currency: String?,
+    val index: Int
+)
