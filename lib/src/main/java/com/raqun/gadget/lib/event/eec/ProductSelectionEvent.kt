e30582973c680fffa1c5158255d952cf18eed9d8
tyln <taylan.sbrcn@gmail.com>
2021-05-13 08:37:27 +0300

    product selection event added

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductSelectionEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductSelectionEvent.kt
@@ -2,10 +2,9 @@ package com.raqun.gadget.lib.event.eec
 
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.raqun.gadget.lib.event.AnalyticsEvent
-import com.raqun.gadget.lib.event.FirebaseEvent
 
-class ProductImpressionEvent(
-    override val name: String = FirebaseAnalytics.Event.VIEW_ITEM_LIST
+class ProductSelectionEvent(
+    override val name: String = FirebaseAnalytics.Event.SELECT_CONTENT
 ) : AnalyticsEvent(), EecEvent {
 
 }
