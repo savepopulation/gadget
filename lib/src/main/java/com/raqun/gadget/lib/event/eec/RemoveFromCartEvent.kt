80e45287e779944488b529466ee3c2fd2d60ca18
tyln <taylan.sbrcn@gmail.com>
2021-05-14 20:58:46 +0300

    remove from cart event implemented eec

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/RemoveFromCartEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/RemoveFromCartEvent.kt
@@ -3,8 +3,6 @@ package com.raqun.gadget.lib.event.eec
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.raqun.gadget.lib.event.AnalyticsEvent
 
-data class RemoveFromCart(
+data class RemoveFromCartEvent(
     override val name: String = FirebaseAnalytics.Event.REMOVE_FROM_CART
-) : AnalyticsEvent() {
-
-}
+) : AnalyticsEvent(), EecEvent
