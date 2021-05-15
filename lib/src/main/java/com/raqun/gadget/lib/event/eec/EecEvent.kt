f79dc84d13184be910fdd6a977ad4ffa2a73d707
tyln <taylan.sbrcn@gmail.com>
2021-05-15 15:38:56 +0300

    reference added to eec events

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/EecEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/EecEvent.kt
@@ -1,4 +1,7 @@
 package com.raqun.gadget.lib.event.eec
 
-// Marker interface for Enhanced e-commerce events
+/*
+ * Marker interface for Enhanced e-commerce events
+ * Ref: https://developers.google.com/tag-manager/android/v5/enhanced-ecommerce
+ */
 interface EecEvent
