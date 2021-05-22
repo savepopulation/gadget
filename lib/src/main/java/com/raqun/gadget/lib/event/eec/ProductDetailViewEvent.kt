0f391fe445217bf2df032f72b4c9b614a4733f5a
tyln <taylan.sbrcn@gmail.com>
2021-05-22 16:33:49 +0300

    product devail event bundle implementation added

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductDetailViewEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductDetailViewEvent.kt
@@ -1,7 +1,11 @@
 package com.raqun.gadget.lib.event.eec
 
+import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.raqun.gadget.lib.domain.Product
 import com.raqun.gadget.lib.event.AnalyticsEvent
+import com.raqun.gadget.lib.extensions.put
+import com.raqun.gadget.lib.extensions.toBundle
 
 /*
  * Product Detail View Event
@@ -9,5 +13,13 @@ import com.raqun.gadget.lib.event.AnalyticsEvent
  * with an item (i.e. product) defined with the relevant fields:
  */
 data class ProductDetailViewEvent(
-    override val name: String = FirebaseAnalytics.Event.VIEW_ITEM
-) : AnalyticsEvent(), EecEvent
+    override val name: String = FirebaseAnalytics.Event.VIEW_ITEM,
+    private val product: Product
+) : AnalyticsEvent(), EecEvent {
+
+    override fun toBundle(): Bundle {
+        return Bundle().apply {
+            put(FirebaseAnalytics.Param.ITEMS, product.toBundle())
+        }
+    }
+}
