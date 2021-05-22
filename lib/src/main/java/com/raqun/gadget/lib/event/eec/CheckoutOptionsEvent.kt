8fd40a75664ab4fe5b857fbe9bfee7bdfd68ef05
tyln <taylan.sbrcn@gmail.com>
2021-05-22 16:28:35 +0300

    checkout option to bundle overridden

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/CheckoutOptionsEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/CheckoutOptionsEvent.kt
@@ -1,7 +1,9 @@
 package com.raqun.gadget.lib.event.eec
 
+import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.raqun.gadget.lib.event.AnalyticsEvent
+import com.raqun.gadget.lib.extensions.put
 
 /*
  * Checkout options
@@ -9,5 +11,15 @@ import com.raqun.gadget.lib.event.AnalyticsEvent
  * with the corresponding CHECKOUT_STEP and CHECKOUT_OPTION parameters:
  */
 data class CheckoutOptionsEvent(
-    override val name: String = FirebaseAnalytics.Event.SET_CHECKOUT_OPTION
-) : AnalyticsEvent(), EecEvent
+    override val name: String = FirebaseAnalytics.Event.SET_CHECKOUT_OPTION,
+    private val checkoutStep: Long,
+    private val checkoutOption: String
+) : AnalyticsEvent(), EecEvent {
+
+    override fun toBundle(): Bundle {
+        return Bundle().apply {
+            put(FirebaseAnalytics.Param.CHECKOUT_STEP, checkoutStep)
+            put(FirebaseAnalytics.Param.CHECKOUT_OPTION, checkoutOption)
+        }
+    }
+}
