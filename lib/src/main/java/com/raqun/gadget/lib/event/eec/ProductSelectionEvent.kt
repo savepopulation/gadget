7377b6aa0d1c0fefaccfabf1612aa7e6aa1ef301
tyln <taylan.sbrcn@gmail.com>
2021-05-22 16:42:42 +0300

    product selection event tobundle implemented

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductSelectionEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/ProductSelectionEvent.kt
@@ -1,10 +1,23 @@
 package com.raqun.gadget.lib.event.eec
 
+import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.raqun.gadget.lib.domain.Product
 import com.raqun.gadget.lib.event.AnalyticsEvent
+import com.raqun.gadget.lib.extensions.put
+import com.raqun.gadget.lib.extensions.putIndex
+import com.raqun.gadget.lib.extensions.toBundle
 
 data class ProductSelectionEvent(
-    override val name: String = FirebaseAnalytics.Event.SELECT_CONTENT
+    override val name: String = FirebaseAnalytics.Event.SELECT_CONTENT,
+    private val product: Product,
+    private val itemListName: String
 ) : AnalyticsEvent(), EecEvent {
 
+    // TODO put index in item list tracker implementation
+    override fun toBundle(): Bundle {
+        return Bundle().apply {
+            put(FirebaseAnalytics.Param.ITEMS, product.toBundle())
+        }
+    }
 }
