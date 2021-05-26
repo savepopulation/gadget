12c4ccc69d3f6ec8dd5a83f18d945ff284a654e6
tyln <taylan.sbrcn@gmail.com>
2021-05-26 14:20:21 +0300

    promotion click event implemented

--- lib/src/main/java/com/raqun/gadget/lib/event/eec/PromotionSelectionEvent.kt
+++ lib/src/main/java/com/raqun/gadget/lib/event/eec/PromotionSelectionEvent.kt
@@ -1,8 +1,12 @@
 package com.raqun.gadget.lib.event.eec
 
+import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.raqun.gadget.lib.Constants
 import com.raqun.gadget.lib.event.AnalyticsEvent
-import com.raqun.gadget.lib.event.FirebaseEvent
+import com.raqun.gadget.lib.extensions.put
+import com.raqun.gadget.lib.extensions.toBundle
+import com.raqun.gadget.lib.model.Promotion
 
 /*
  * Promotion clicks/selections
@@ -10,5 +14,17 @@ import com.raqun.gadget.lib.event.FirebaseEvent
  * with a promotion defined with the relevant fields:
  */
 data class PromotionSelectionEvent(
-    override val name: String = FirebaseAnalytics.Event.SELECT_CONTENT
-) : AnalyticsEvent(), EecEvent
+    override val name: String = FirebaseAnalytics.Event.SELECT_CONTENT,
+    private val promotions: List<Promotion>,
+    private val promotionId: String,
+    private val contentType: String
+) : AnalyticsEvent(), EecEvent {
+
+    override fun toBundle(): Bundle {
+        return Bundle().apply {
+            putParcelableArrayList(Constants.Param.PROMOTIONS, promotions.toBundle())
+            put(FirebaseAnalytics.Param.ITEM_ID, promotionId)
+            put(FirebaseAnalytics.Param.CONTENT_TYPE, contentType)
+        }
+    }
+}
