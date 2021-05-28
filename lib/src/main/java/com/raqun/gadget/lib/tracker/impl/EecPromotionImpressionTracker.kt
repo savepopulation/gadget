a9c04a57dff6d34f6f7e859a3859dba886c9fb31
tyln <taylan.sbrcn@gmail.com>
2021-05-28 23:18:16 +0300

    promotion impression tracker improved to support lifecycle events

--- lib/src/main/java/com/raqun/gadget/lib/tracker/impl/EecPromotionImpressionTracker.kt
+++ lib/src/main/java/com/raqun/gadget/lib/tracker/impl/EecPromotionImpressionTracker.kt
@@ -1,10 +1,14 @@
 package com.raqun.gadget.lib.tracker.impl
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import com.raqun.gadget.lib.Gadget
 import com.raqun.gadget.lib.event.eec.PromotionImpressionEvent
 import com.raqun.gadget.lib.event.eec.PromotionSelectionEvent
 import com.raqun.gadget.lib.model.Promotion
 import com.raqun.gadget.lib.tracker.AnalyticsTracker
+import com.raqun.gadget.lib.tracker.LifecycleEventTracker
 import com.raqun.gadget.lib.tracker.ListItemImpressionTracker
 
 /*
@@ -12,11 +16,18 @@ import com.raqun.gadget.lib.tracker.ListItemImpressionTracker
  * Can be used to track promotion impressions and selections for EEC
  */
 class EecPromotionImpressionTracker(
+    override val lifeCycle: Lifecycle,
     override val analyticsTracker: AnalyticsTracker = Gadget,
     override val viewedItems: LinkedHashSet<Promotion> = linkedSetOf(),
     private val eventName: String,
-    private val contentType: String
-) : ListItemImpressionTracker<Promotion> {
+    private val contentType: String,
+) : ListItemImpressionTracker<Promotion>,
+    LifecycleObserver,
+    LifecycleEventTracker {
+
+    init {
+        lifeCycle.addObserver(this)
+    }
 
     override fun trackItemSelection(item: Promotion) {
         val promotions = ArrayList<Promotion>()
@@ -31,6 +42,7 @@ class EecPromotionImpressionTracker(
         analyticsTracker.track(promotionSelectionEvent)
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     override fun trackImpression() {
         if (viewedItems.isNullOrEmpty()) return
         val promotionImpressionEvent = PromotionImpressionEvent(
